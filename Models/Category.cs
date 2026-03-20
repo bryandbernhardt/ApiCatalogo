6e1fb7916786d777fb2a74090ebf91b15bb9fe96
bryandbernhardt <bryandbernhardt@gmail.com>
2026-03-20 01:07:07 -0300

    Apply updated .gitignore rules

---
+++ Models/Category.cs
@@ -0,0 +1,24 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Models;
+
+public class Category
+{
+  public Category()
+  {
+    Products = new Collection<Product>();
+  }
+  
+  public int Id { get; set; }
+  
+  [Required]
+  [StringLength(80)]
+  public string? Name { get; set; }
+  
+  [Required]
+  [StringLength(300)]
+  public string? ImageUrl { get; set; }
+  
+  public ICollection<Product>? Products { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using System.Text.Json.Serialization;
+using ApiCatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(defaultConnection,
+    ServerVersion.AutoDetect(defaultConnection)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/swagger/v1/swagger.json");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
